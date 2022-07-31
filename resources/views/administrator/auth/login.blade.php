dd3412cbbcd263ca710b96efdf599aa6a50234e2
devProMaleek <milikiadbay@gmail.com>
2022-07-31 02:04:33 +0100

    feat: Admin Register Page
    Successfully create the Admin Register Page

--- resources/views/administrator/auth/login.blade.php
+++ resources/views/administrator/auth/login.blade.php
@@ -47,7 +47,7 @@
         <div class="block mt-4">
           <label for="remember_me" class="inline-flex items-center">
             <input id="remember_me" type="checkbox"
-              class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              class="text-purple-600 form-checkbox focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
               name="remember">
             <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
           </label>
---
+++ resources/views/components/admin-create-account-form.blade.php
@@ -0,0 +1,10 @@
+<div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+    <div class="w-full">
+      <h1
+        class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200"
+      >
+        Create an Admin account
+      </h1>
+      {{ $slot }}
+    </div>
+</div>
